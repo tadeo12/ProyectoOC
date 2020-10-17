329255e858ec04585f69c241833a8bb4cba0d7dc
Nicolas <nicomleidi@gmail.com>
2020-10-16 21:18:21 -0300

    Agrego elementos y veo su orden

--- main.c
+++ main.c
@@ -9,15 +9,20 @@ void test_lista(){
     crear_lista(&lista);
     tPosicion p = l_primera(lista);
     char *m, n = 'A';
+    char n2 = 'C';
+    char n3 = 'B';
     l_insertar(lista, p, &n);
     m = (char*) (l_recuperar(lista, l_primera(lista)));
     printf("Primero %c\n", *m);
-    char n2 = 'C';
+    printf("Inserto B y C\n");
     l_insertar(lista, l_fin(lista), &n2);
-    m = (char*) (l_recuperar(lista, l_ultima(lista)));
-    printf("Ultimo %c\n", *m);
+    l_insertar(lista, l_ultima(lista), &n3);
     m = (char*) (l_recuperar(lista, l_primera(lista)));
     printf("Primero %c\n", *m);
+    m = (char*) (l_recuperar(lista, l_siguiente(lista, l_primera(lista))));
+    printf("Segundo %c\n", *m);
+    m = (char*) (l_recuperar(lista, l_ultima(lista)));
+    printf("Ultimo %c\n", *m);
 }
 
 int main()
