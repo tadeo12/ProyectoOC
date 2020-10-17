b0778d44d9a75a8e7597687e43e0a7a4281da86a
ValenAle612 <valenale612@gmail.com>
2020-10-16 21:30:39 -0300

    EO EEEEEEEO

--- mapeo.c
+++ mapeo.c
@@ -1,3 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "mapeo.h"
+
+
+void crear_mapeo(tMapeo * m, int ci, int (*fHash)(void *), int (*fComparacion)(void *, void *)){
+    //CODIGO
+}
+
+tValor m_insertar(tMapeo m, tClave c, tValor v){
+    //CODIGO
+}
+
+void m_eliminar(tMapeo m, tClave c, void (*fEliminarC)(void *), void (*fEliminarV)(void *)){
+    //CODIGO
+}
+
+void m_destruir(tMapeo * m, void (*fEliminarC)(void *), void (*fEliminarV)(void *)){
+    //CODIGO
+}
+
+tValor m_recuperar(tMapeo m, tClave c){
+    //CODIGO
+}
