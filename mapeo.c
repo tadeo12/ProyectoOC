0a5648f6eb21aeb20815720ee62da03594564018
Nicolas <nicomleidi@gmail.com>
2020-10-19 20:40:35 -0300

    Constructor y recuperar

--- mapeo.c
+++ mapeo.c
@@ -8,10 +8,12 @@ void crear_mapeo(tMapeo * m, int ci, int (*fHash)(void *), int (*fComparacion)(v
     *m = malloc(sizeof(tMapeo));
     if(*m == NULL)
         exit(MAP_ERROR_MEMORIA);
-    (*m) -> longitud_tabla = (10 < ci ? ci : 10);   //La longitud de la tabla es inicialmente el mayor entre 10 y ci
+    (*m) -> longitud_tabla = (10 < ci ? ci : 10);                               //La longitud de la tabla es inicialmente el mayor entre 10 y ci
     (*m) -> hash_code = fHash;
     (*m) -> comparador = fComparacion;
-    crear_lista((*m) -> tabla_hash);                //Creo una lista pasándole el puntero de la lista
+    (*m) -> tabla_hash = malloc( ((*m) -> longitud_tabla) * sizeof(tLista));    //Creo los buckets
+    for(int i = 0; i < (*m) -> longitud_tabla; i++)
+        crear_lista((*m) -> tabla_hash + i);                                    //Creo las listas en los buckets
 }
 
 tValor m_insertar(tMapeo m, tClave c, tValor v){
@@ -27,5 +29,17 @@ void m_destruir(tMapeo * m, void (*fEliminarC)(void *), void (*fEliminarV)(void 
 }
 
 tValor m_recuperar(tMapeo m, tClave c){
-    //TODO
+    int claveHash = m -> hash_code(c) % m -> longitud_tabla;
+    tLista bucket = *((m -> tabla_hash) + claveHash);
+    tPosicion p = l_primera(bucket);
+    tValor aRetornar = NULL;
+    tEntrada entrada = (tEntrada) l_recuperar(bucket, p);
+    while(l_siguiente(bucket, p) != NULL && m -> comparador(entrada -> clave, c) == 0){     //Hasta que termine de ver todo el bucket o lo encuentre
+        p = l_siguiente(bucket, p);
+        entrada = (tEntrada) l_recuperar(bucket, p);
+    }
+    if(entrada != NULL && m -> comparador(entrada -> clave, c) == 0){                       //Si es la entrada correcta tiene la clave correcta
+       aRetornar = entrada -> valor;
+    }
+    return aRetornar;
 }
