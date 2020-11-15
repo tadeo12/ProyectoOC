b737fff603ed4eb509ad40ecd914506fa24d1b0e
tadeo12 <71443379+tadeo12@users.noreply.github.com>
2020-11-15 12:31:52 -0300

    Update mapeo.c

--- mapeo.c
+++ mapeo.c
@@ -1,101 +1,157 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
-#include "lista.h"
 #include "mapeo.h"
+#include "lista.h"
 
-// Macros con códigos de finalización
-#define ERROR_APERTURA_ARCHIVO        -1
-#define ERROR_INVOCACION_PROGRAMA     -2
 
-int fCompararPalabras(void * a, void * b){
-    char* A = ( (char*) a);
-    char* B = ( (char*) b);
-    return strcmp(A,B);
-}
+void (*funcionEliminarClave)(void *)= NULL;
+void (*funcionEliminarValor)(void *)= NULL;
 
-int fHashPalabras(void * a){
-    char* str = ((char*) a);
-    int c, hash =5381;
-
-    while (*str){
-        str++;
-        c = *str;
-        hash = ((hash << 5) + hash) + c;    //hash * 33 + el caracter
+void crear_mapeo(tMapeo * m, int ci, int (*fHash)(void *), int (*fComparacion)(void *, void *)){
+    *m = malloc(sizeof(struct mapeo));
+    if(*m == NULL){
+        printf("ERROR: No se pudo reservar espacio en memoria.");
+        exit(MAP_ERROR_MEMORIA);
     }
-    hash = hash < 0 ? -hash : hash;         //Si llega a ser negativo
-    return hash;
+    (*m) -> longitud_tabla = (10 < ci ? ci : 10);       //la longitud minima es 10
+    (*m) -> cantidad_elementos = 0;
+    (*m) -> hash_code = fHash;
+    (*m) -> comparador = fComparacion;
+    (*m) -> tabla_hash = malloc( ((*m) -> longitud_tabla) * sizeof(tLista));
+    for(int i = 0; i < (*m) -> longitud_tabla; i++)
+        crear_lista((*m) -> tabla_hash + i);            //creo cada bucket
 }
 
-void fEliminarPalabra(void * a){
-    free(a);
+void fEliminarEntrada(tElemento entrada){
+    tEntrada entry = (tEntrada) entrada;
+    funcionEliminarClave(entry -> clave);         //elimino la clave de la entrada
+    funcionEliminarValor(entry -> valor);         //enlimino el valor de la entrada
+    free(entrada);
 }
 
-
-void fEliminarInt(void * a){
-    free(a);
+static void insertarInterno(tMapeo m, tEntrada* entrada){
+    int claveHash = ( m -> hash_code((*entrada) -> clave) ) % ( m -> longitud_tabla );              //obtengo el hashCode
+    l_insertar(*(m -> tabla_hash + claveHash), l_fin(*(m -> tabla_hash + claveHash)) , *entrada);   //inserto al final de bucket
 }
 
-void leerPalabras(FILE * archivo, tMapeo m){
-    int * cantidad;
-    int * valorAnterior;
-    char * palabra = malloc(30 * sizeof(char));
-    while(!feof(archivo)){
-        fscanf(archivo, "%30s[^\n] ", palabra);
-        if(!feof(archivo))
-            fgetc(archivo);                                 //consume espacios entre palabras
-        cantidad = malloc(sizeof(int));
-        valorAnterior =(int *)m_recuperar(m, palabra);
-        if(valorAnterior != NULL)
-            (*cantidad) = (*valorAnterior) + 1;
-        else{
-            (*cantidad) = 1;
-            free(valorAnterior);                            //libero el espacio ocupado por la cantidad anterior
+void fEliminarNada(tElemento entrada){          //Método para que no elimina las entradas, lo usa el redimencsionar
+}
+
+void redimensionar(int longitud, tMapeo m){
+    tLista* tablaHashVieja = (m -> tabla_hash);
+
+    m -> tabla_hash = malloc( longitud * sizeof( tLista ) );
+    tPosicion pos;
+    int largo, largoAnterior = m -> longitud_tabla;
+    m -> longitud_tabla = longitud;
+    for(int i = 0 ; i < longitud ; i++){
+        crear_lista(m -> tabla_hash + i);                                   //creo los nuevos buckets
+    }
+    for(int i = 0; i < largoAnterior ; i++){
+        pos = l_primera(*(tablaHashVieja + i));                             //guardo la primera posicion de cada bucket (lista)
+        largo = l_longitud(*(tablaHashVieja + i));                          //obtengo la longitud del bucket
+        for(int j = 0; j < largo; j++){
+            tEntrada entrada = l_recuperar(*(tablaHashVieja + i), pos);     //obtengo la entrada en cada bucket
+            insertarInterno(m, &entrada);
+            if(j < largo - 1)
+                pos = l_siguiente(*(tablaHashVieja + i), pos);
         }
-        m_insertar(m,palabra,cantidad);
-        palabra = malloc(30 * sizeof(char));                //reservo espacio para nueva palabra
+        l_destruir(tablaHashVieja + i, fEliminarNada);
     }
+    free(tablaHashVieja);
+
+}
+
+tValor m_insertar(tMapeo m, tClave c, tValor v){
+    tValor toReturn = NULL;
+    int longitud = (m-> longitud_tabla);
+    int cantElem = (m-> cantidad_elementos), largo, encontre = 0;
+    if((cantElem / longitud) >= (0.75)){                                //Checkeo si supero el factor de carga
+        redimensionar(longitud * 2,m);                                  //Duplico el tamaño del mapeo
+    }
+    int claveHash = ( m -> hash_code(c) ) % ( m -> longitud_tabla );
+    tLista lista = *(m -> tabla_hash + claveHash);
+    largo = l_longitud(lista);
+    tPosicion actual = l_primera(lista);
+    tEntrada entrada;
+    for(int i = 0; i < largo && !encontre; i++){
+        entrada = l_recuperar(lista, actual);
+        if(( m -> comparador( (entrada -> clave), c) ) == 0){           //Si lo encuentro, actualizo su valor y retorno el anterior
+            encontre = 1;
+            toReturn = entrada -> valor;
+            entrada -> valor = v;
+        }
+        if(i < longitud - 1)
+            actual = l_siguiente(lista, actual);
+    }
+
+    if(!encontre){
+        entrada = malloc(sizeof(struct entrada));
+        if(entrada == NULL){
+            printf("ERROR: No se pudo reservar memoria.");
+            exit(MAP_ERROR_MEMORIA);
+        }
+        entrada -> clave = c;
+        entrada -> valor = v;
+        l_insertar(*(m -> tabla_hash + claveHash), l_fin(*(m -> tabla_hash + claveHash)) , entrada);
+        m -> cantidad_elementos++;
+    }
+    return toReturn;
 }
 
 
-int main(int argc, char *argv[]){
-    tMapeo mapeo;
-    int * repeticiones;
-    int opcion;
-    char * ruta_archivo, * palabra;
-    FILE * archivo;
-    if(argc!=2){
-        printf("ERROR: Cantidad de argumentos equivocado");
-        return ERROR_INVOCACION_PROGRAMA;
-    }
-    ruta_archivo = argv[1];
-    palabra = malloc(sizeof(char)*30);
-    archivo = fopen(ruta_archivo,"r");
-    if(archivo == NULL){
-        printf("ERROR: Archivo no encontrado");
-        return ERROR_APERTURA_ARCHIVO;
-    }
-    crear_mapeo(&mapeo,20,&fHashPalabras,&fCompararPalabras);
-    leerPalabras(archivo,mapeo);                                            //Leo las palabras del archivo
 
-    printf("Archivo leido\n");
-    printf("Menu de operaciones \n");
-    printf("Consultar cantidad de apariciones(1) o salir(2) ?\n");
-    scanf("%d",&opcion);
-    while(opcion == 1){
-        printf("Ingrese la palabra a buscar \n");
-        scanf("%s", palabra);
-        repeticiones = (int *)m_recuperar(mapeo,palabra);                  //Recupero la cantidad de apariciones, NULL si no está
-        if (repeticiones)
-            printf("la palabra %s aparece %d veces en el archivo \n",palabra, *repeticiones);
-        else
-            printf("la palabra %s no aparece en el archivo \n",palabra);
-        printf("consultar cantidad de apariciones(1) o salir(2) ?\n");
-        scanf("%d",&opcion);
+void m_eliminar(tMapeo m, tClave c, void (*fEliminarC)(void *), void (*fEliminarV)(void *)){
+    int valorHash = m -> hash_code(c) % (m -> longitud_tabla);
+    tLista bucket= *(m -> tabla_hash + valorHash );
+    tPosicion pos= l_primera(bucket);
+    tEntrada entrada;
+    int largo = l_longitud(bucket);
+    int encontre = 0;
+
+    for(int i = 0; i < largo && !encontre; i++){
+        entrada = l_recuperar(bucket, pos);
+        if( m -> comparador( (entrada -> clave), c) == 0){
+            encontre = 1;
+            funcionEliminarClave=fEliminarC;
+            funcionEliminarValor=fEliminarV;
+            l_eliminar(bucket,pos,fEliminarEntrada);                //Si encuentro la entrada, la elimino
+            m -> cantidad_elementos--;
+        }
+        if(i < largo - 1)
+            pos = l_siguiente(bucket, pos);
     }
-    free(palabra);                                                          //Libero la palabra que la reservé al inicio
-    m_destruir(&mapeo,&fEliminarPalabra,&fEliminarInt);
-    fclose(archivo);
-    return 0;
+}
+
+void m_destruir(tMapeo * m, void (*fEliminarC)(void *), void (*fEliminarV)(void *)){
+    tLista* lista = (*m) -> tabla_hash;
+    for(int i = 0; i < (*m) -> longitud_tabla; i++){
+        funcionEliminarClave=fEliminarC;
+        funcionEliminarValor=fEliminarV;
+        l_destruir(lista + i, fEliminarEntrada);            //Elimino todas las listas con sus entradas
+    }
+    free((*m) -> tabla_hash);                               //Libero el arreglo de listas
+    free(*m);                                               //Libero el mapeo
+    *m = NULL;
+}
+
+tValor m_recuperar(tMapeo m, tClave c){
+    int claveHash = m -> hash_code(c) % m -> longitud_tabla;
+    tLista bucket = *((m -> tabla_hash) + claveHash);
+    tPosicion p = l_primera(bucket);
+    tValor aRetornar = NULL;
+    tEntrada entrada;
+    int largo = l_longitud(bucket), encontre = 0;
+
+    for(int i = 0; i < largo && !encontre; i++){
+        entrada = l_recuperar(bucket, p);                       //Por cada elemento en el bucket correspondiente checkeo si es el que busco
+        if( m -> comparador((entrada -> clave), c) == 0){
+            encontre = 1;
+            aRetornar = entrada -> valor;
+        }
+        if(i < largo - 1)                                       //En la última vuelta no tiene que ir al siguiente (se sale de la lista si hace siguiente del ultimo)
+            p = l_siguiente(bucket, p);
+    }
+    return aRetornar;
 }
 
